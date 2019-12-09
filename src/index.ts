dad32c07330fe4a6f2191caeb0ec43f868a2f7af
xusanduo08 <2279190887@qq.com>
2019-12-09 20:37:13 +0800

    更改校验方法

--- src/index.ts
+++ src/index.ts
@@ -2,92 +2,43 @@
 // - https://umijs.org/plugin/develop.html
 import { IApi } from 'umi-types';
 import validate from './utils/validate';
+import uploadDir from './uploadDir';
+import uploadFile from './uploadFile';
 const fs = require('fs');
 const path = require('path');
-const client = require('scp2');
-const ssh2 = require('ssh2');
-const {Client} = ssh2;
+
 
 export default function (api: IApi, options) {
-  let {targetPath, sourcePath, host, username, password} = options;
-  console.log(path.resolve(sourcePath));
+  let {targetPath, sourcePath, host, username} = options;
   
-
   api.onBuildSuccess(() => {
 
     // TODO 如果没有密码，提示输入密码
 
-    validate(host, [{name:'type', value:'string'}, {name:'required'}])
-    validate(username, [{name:'type', value:'string'}, {name:'required'}])
-    
+    validate({validateTarget:host, validateName:'Host', validateItems: [{type:'type', value:'string'}, {type:'required'}]});
+    validate({validateTarget: username, validateName:'Username', validateItems: [{type:'type', value:'string'}, {type:'required'}]});
+
     // TODO 校验是否是有效的host
 
-    validate(sourcePath, [{name:'type', value: 'string'}])
-    
-    validate(targetPath, [
-      {name:'type', value: 'string'},
-      {name:'required'},
-      {name:'absolute path', rule:(target)=>{
-        if(!target.startsWith('/')){
-          return false;
-        }
-        return true;
-      }}
-    ]);
+    validate({validateTarget: sourcePath, validateName:'SourcePath', validateItems: [{type: 'required'}, {type:'type', value: 'string'}]});
+    validate({validateTarget: targetPath,
+      validateName:'TargetPath',
+      validateItems: [
+        {type:'type', value: 'string'},
+        {type:'required'},
+        {type:'absolute path', rule:(target)=>{
+          if(!target.startsWith('/')){
+            return false;
+          }
+          return true;
+        }}
+      ]
+    })
 
     if(fs.statSync(path.resolve(sourcePath)).isDirectory()){
-      // TODO 上传文件夹
+      uploadDir(options);
     } else {
-      // TODO 上传文件夹
+      uploadFile(options);
     }
-    
-    
-    
-    // TODO 处理目标路径结尾的斜杠/
-    if(targetPath[targetPath.length - 1] === '/'){
-      targetPath = targetPath.slice(0, targetPath.length - 1);
-    }
-
-    const rootPath = targetPath.slice(0, targetPath.lastIndexOf('/')); // 获取文件存放目录的上一层
-    const dirName = targetPath.slice(targetPath.lastIndexOf('/') + 1); // 获取文件存放目录名
-    const tempDirName = Date.now(); // 临时目录
-    console.log(rootPath, dirName, tempDirName);
-    console.log('Uploading...');
-    client.scp(sourcePath, {
-      host: host,
-      username: username,
-      password: password,
-      path: `${targetPath}/../${tempDirName}`
-    }, err => {
-      if(err){
-        console.log('Upload failed!');
-        console.error(err);
-        fs.appendFileSync('log.txt', err, 'utf8', err => {
-          console.log(err);
-        })
-        process.exit(1);
-      }
-      let conn = new Client();
-      conn.on('ready', function(){
-        // 1. 进入文件目录所在路径
-        // 2. 删除已存在文件目录
-        // 3. 将dist重命名成制定目录名
-        conn.exec(`cd ${rootPath};rm -rf ${dirName};mv ${tempDirName} ${dirName}`, function(err, stream){
-          if(err){
-            throw err;
-          }
-          stream.on('close', function(){
-            console.log('Upload success !')
-            conn.end();
-          }).on('data', function(data){
-            console.log('stdout:: ' + data);
-          })
-        })
-      }).connect({
-        host: host,
-        username: username,
-        password: password
-      })
-    })
   });
 } 
--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,34 +1,35 @@
 type rule = (t:any) => Boolean;
-type checkItem = {name: string, value?: string, rule?: rule};
+type checkItem = {type: string, value?: string, rule?: rule};
+type validateItem = {validateTarget: any, validateName: string, validateItems:checkItem[]};
 
-const validate = function(valid: any, checkItems: checkItem[]):void{
-  
-  for(let i = 0; i < checkItems.length; i++){
-    let item = checkItems[i];
-    let {name, value, rule} = item;
-    if(!name){
-      console.log('Lack of validate name !');
+const validate = function(v:validateItem):void{
+  let {validateTarget, validateName, validateItems} = v;
+  for(let i = 0; i < validateItems.length; i++){
+    let item = validateItems[i];
+    let {type, value, rule} = item;
+    if(!type){
+      console.log('Lack of validate type !');
       continue;
     }
-    switch(name){
+    switch(type){
       case 'type':
-        if(typeof valid !== value){
-          throw `${name} should be ${value}!`;
+        if(typeof validateTarget !== value){
+          throw `${validateName} should be ${value}!`;
         }
-        console.log(`Validate ${name}: true`);
+        console.log(`Validate ${validateName}: true`);
         break;
       case 'required':
-        if(!valid){
-          throw `${name} is Required!`;
+        if(!validateTarget){
+          throw `${validateName} is Required!`;
         }
-        console.log(`Validate ${name}: true`);
+        console.log(`Validate ${validateName}: true`);
         break;
       default:
         if(rule){
-          if(!rule(valid)){
-            throw `Vlidate ${name} failed !`;
+          if(!rule(validateTarget)){
+            throw `Vlidate ${validateName} failed !`;
           }
-          console.log(`Validate ${name}: true`);
+          console.log(`Validate ${validateName}: true`);
         }
         break;
     }
