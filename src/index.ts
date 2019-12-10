2a184aea314823c3986404c70d094dacfa25effb
xusanduo08 <2279190887@qq.com>
2019-12-10 21:44:33 +0800

    路径分隔符统一成unix系统下的斜杠

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,16 @@ import { IApi } from 'umi-types';
 import validate from './utils/validate';
 import uploadDir from './uploadDir';
 import uploadFile from './uploadFile';
+import unixy from './utils/unixy';
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
 
 export default function (api: IApi, options) {
+  options.targetPath = unixy(options.targetPath);
+  options.sourcePath = unixy(options.sourcePath);
   let {targetPath, sourcePath, host, username, password} = options;
-
+  
   api.onBuildSuccess(() => {
     validate({validateTarget:host, validateName:'Host', validateItems: [{type:'type', value:'string'}, {type:'required'}]});
     validate({validateTarget: username, validateName:'Username', validateItems: [{type:'type', value:'string'}, {type:'required'}]});
